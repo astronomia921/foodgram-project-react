931e0ddea1df055b64c94463ef6cf71b9a932a39
Danil Mustafin <danil.mustafin921@gmail.com>
2023-07-07 21:42:34 +0500

    csv

--- backend/foodgram_backend/apps/ingredients/management/commands/load_data.py
+++ backend/foodgram_backend/apps/ingredients/management/commands/load_data.py
@@ -1,4 +1,4 @@
-import json
+import csv
 
 from django.core.management.base import BaseCommand
 
@@ -6,19 +6,28 @@ from apps.ingredients.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка данных из JSON файла в таблицу Ingredients'
+    help = 'Импорт данных из csv в модель Ingredient'
 
     def add_arguments(self, parser):
-        parser.add_argument('--path', type=str, help='Путь до JSON файла')
+        parser.add_argument('--path', type=str, help='Путь к файлу')
 
     def handle(self, *args, **options):
-        file_path = options['path'] + 'ingredients.json'
-        with open(file_path, encoding="utf8") as f:
-            data = json.load(f)
-            for item in data:
-                Ingredient.objects.create(
-                    name=item['name'],
-                    measurement_unit=item['measurement_unit']
-                )
-        self.stdout.write(
-            self.style.SUCCESS('Загрузка данных прошла успешно.'))
+        print('Заполнение модели Ingredient из csv запущено.')
+        file_path = options['path'] + 'ingredients.csv'
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+
+            for row in reader:
+                try:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=row[0],
+                        measurement_unit=row[1],
+                    )
+                    if not created:
+                        print(
+                            f'Ингредиент {obj} уже существует в базе данных.'
+                        )
+                except Exception as error:
+                    print(f'Ошибка в строке {row}: {error}')
+
+        print('Заполнение модели Ingredient завершено.')
